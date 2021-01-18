5695900dbd4b624873f680842af2ab936d227f33
Richard Willian <richardwillian.2008@hotmail.com>
2021-01-18 06:09:42 -0300

    Adiciona função para extrair informações de exercícios tipo "selection"

--- src/tipos_exercicios/tipo_selection.js
+++ src/tipos_exercicios/tipo_selection.js
@@ -1,164 +1,186 @@
 const puppeteer = require("puppeteer");
+const utils = require("../utils")
 
-(async() => {
-    var urls_selection = [
-        'https://test-english.com/grammar-points/a1/present-simple-forms-of-to-be',
-    ]
+function recuperarQuestoesSelection() {
 
-    let url = urls_selection[0];
-    let browser = await puppeteer.launch();
-    let page = await browser.newPage();
+    return new Promise(async function(resolve) {
 
-    await page.goto(url, { waitUntil: "networkidle2" });
+        let paginas = [];
 
-    let data = await page.evaluate(async() => {
+        let urls_selection = [
+            'https://test-english.com/grammar-points/a1/present-simple-forms-of-to-be',
+            'https://test-english.com/grammar-points/a1/this-that-these-those/'
+        ]
 
-        function delay(time) {
-            return new Promise(function(resolve) {
-                setTimeout(resolve, time)
-            });
-        }
+        urls_selection.forEach(async(url) => {
 
-        function recuperarObjDropdown(valorDropdown, exercicio, item) {
-            var nomeDropdown = `(dropdown${valorDropdown})`;
-            exercicio.questao += nomeDropdown;
+            console.log(`Início Scrapping: ${url}`);
 
-            var objDropdown = {
-                nome: nomeDropdown,
-                opcoes: []
-            };
+            let browser = await puppeteer.launch();
+            let page = await browser.newPage();
+            await page.goto(url, { waitUntil: "networkidle2" });
 
-            item.children.forEach(function(option) {
-                if (option.innerHTML) {
-                    objDropdown.opcoes.push({ valor: option.innerHTML });
+            let data = await page.evaluate(async() => {
+
+                function delay(time) {
+                    return new Promise(function(resolve) {
+                        setTimeout(resolve, time)
+                    });
                 }
-            });
 
-            return objDropdown;
-        }
+                function recuperarObjetoDropdown(valorDropdown, exercicio, item) {
+                    var nomeDropdown = `(dropdown${valorDropdown})`;
+                    exercicio.questao += nomeDropdown;
 
-        var objLog = {};
-
-        async function recuperarRespostas() {
-
-            objLog.peloMenosChegouAqui2 = "??";
-
-            objLog.testando = true;
-
-            $("#action-button").click();
-
-            await delay(4000);
-
-            $(".feedback-incorrect").each(function(index, item) {
-
-                objLog.peloMenosChegouAqui3 = "???";
-
-                var paragrafo = $(item).find("p")[0];
-                var filhosParagrafo = paragrafo.childNodes;
-                var linhaResposta = $(".findMe", filhosParagrafo).prevObject[0];
-                var textoLinhaResposta = $(linhaResposta)[0].innerHTML;
-                var respostas = textoLinhaResposta.replace("Correct answers: ", "").replace("Correct answer: ", "");
-
-                var arrayRespostas = respostas.split("/");
-
-                arrayRespostas.forEach(function(resp, i) {
-
-                    objLog = {
-                        "exercicio": index,
-                        "resposta": resp,
-                        "indexResposta": i,
-                        "dropdownCorrente": pagina.exercicios[index].dropdowns[i]
+                    var objDropdown = {
+                        nome: nomeDropdown,
+                        opcoes: []
                     };
 
-                    pagina.exercicios[index].dropdowns[i].opcoes.find((opc, ind) => {
-
-                        objLog.opcao = opc;
-
-                        if (opc.valor.toLowerCase() === resp.toLowerCase()) {
-                            objLog.dropSeraModificada = pagina.exercicios[index].dropdowns[i].opcoes[ind];
-                            pagina.exercicios[index].dropdowns[i].opcoes[ind].correta = true;
-                            objLog.dropAposModificada = pagina.exercicios[index].dropdowns[i].opcoes[ind];
-                            return true;
+                    item.children.forEach(function(option) {
+                        if (option.innerHTML) {
+                            objDropdown.opcoes.push({ valor: option.innerHTML });
                         }
                     });
-                });
-            });
 
-            objLog.peloMenosChegouAqui1 = "?";
-        }
-
-        pagina = {
-            titulo: $("#the_title_h1").text().replace("\n", ""),
-            texto_exercicio: $($("#exercises").find("h3")[0]).text(),
-            enunciado: $($("#exercises").find("h5")[0]).text(),
-            exercicios: []
-        }
-
-        async function recuperarQuestoes() {
-            var formulario = document.getElementsByClassName("quiz-form")[0];
-            var questoesFormulario = formulario.getElementsByClassName("watu-question");
-
-            questoesFormulario.forEach(function(questao, index) {
-
-                var paragrafos = $(questao).find("p");
-
-                var exercicio = {
-                    numero: index + 1,
-                    tipo: "",
-                    questao: "",
-                    dropdowns: []
+                    return objDropdown;
                 }
 
-                for (var i = 0; i < paragrafos.length; i++) {
+                var objLog = {};
 
-                    var paragrafo = paragrafos[i];
-                    var elementosDentroParagrafo = paragrafo.childNodes;
-                    var elementosTagHTML = $('.FindMe', elementosDentroParagrafo);
-                    var itens = elementosTagHTML.prevObject;
+                async function recuperarRespostas() {
 
-                    var valorDropdown = 0;
+                    objLog.peloMenosChegouAqui2 = "??";
 
-                    for (var j = 0; j < itens.length; j++) {
-                        var item = $(itens[j])[0];
+                    objLog.testando = true;
 
-                        switch (item.nodeName) {
-                            case "SPAN":
-                                // exercicio.questao += item.innerHTML + " - ";
-                                break;
-                            case "#text":
-                                exercicio.questao += item.nodeValue;
-                                break;
-                            case "SELECT":
+                    $("#action-button").click();
 
-                                exercicio.tipo = "selection";
-                                valorDropdown++;
+                    await delay(4000);
 
-                                var objDropdown = recuperarObjDropdown(valorDropdown, exercicio, item);
+                    $(".feedback-incorrect").each(function(index, item) {
 
-                                exercicio.dropdowns.push(objDropdown);
-                                break;
-                            case "INPUT":
-                                exercicio.questao += "(input)"
-                                break;
+                        objLog.peloMenosChegouAqui3 = "???";
+
+                        var paragrafo = $(item).find("p")[0];
+                        var filhosParagrafo = paragrafo.childNodes;
+                        var linhaResposta = $(".findMe", filhosParagrafo).prevObject[0];
+                        var textoLinhaResposta = $(linhaResposta)[0].innerHTML;
+                        var respostas = textoLinhaResposta.replace("Correct answers: ", "").replace("Correct answer: ", "");
+
+                        var arrayRespostas = respostas.split("/");
+
+                        arrayRespostas.forEach(function(resp, i) {
+
+                            objLog = {
+                                "exercicio": index,
+                                "resposta": resp,
+                                "indexResposta": i,
+                                "dropdownCorrente": pagina.exercicios[index].dropdowns[i]
+                            };
+
+                            pagina.exercicios[index].dropdowns[i].opcoes.find((opc, ind) => {
+
+                                objLog.opcao = opc;
+
+                                if (opc.valor.toLowerCase() === resp.toLowerCase()) {
+                                    objLog.dropSeraModificada = pagina.exercicios[index].dropdowns[i].opcoes[ind];
+                                    pagina.exercicios[index].dropdowns[i].opcoes[ind].correta = true;
+                                    objLog.dropAposModificada = pagina.exercicios[index].dropdowns[i].opcoes[ind];
+                                    return true;
+                                }
+                            });
+                        });
+                    });
+
+                    objLog.peloMenosChegouAqui1 = "?";
+                }
+
+                pagina = {
+                    titulo: $("#the_title_h1").text().replace("\n", ""),
+                    texto_exercicio: $($("#exercises").find("h3")[0]).text(),
+                    enunciado: $($("#exercises").find("h5")[0]).text(),
+                    exercicios: []
+                }
+
+                async function recuperarQuestoes() {
+                    var formulario = document.getElementsByClassName("quiz-form")[0];
+                    var questoesFormulario = formulario.getElementsByClassName("watu-question");
+
+                    questoesFormulario.forEach(function(questao, index) {
+
+                        var paragrafos = $(questao).find("p");
+
+                        var exercicio = {
+                            numero: index + 1,
+                            tipo: "selection",
+                            questao: "",
+                            dropdowns: []
                         }
-                    }
 
-                    pagina.exercicios.push(exercicio);
+                        for (var i = 0; i < paragrafos.length; i++) {
+
+                            var paragrafo = paragrafos[i];
+                            var elementosDentroParagrafo = paragrafo.childNodes;
+                            var elementosTagHTML = $('.FindMe', elementosDentroParagrafo);
+                            var itens = elementosTagHTML.prevObject;
+
+                            var valorDropdown = 0;
+
+                            for (var j = 0; j < itens.length; j++) {
+                                var item = $(itens[j])[0];
+
+                                switch (item.nodeName) {
+                                    case "#text":
+                                        exercicio.questao += item.nodeValue;
+                                        break;
+                                    case "SELECT":
+                                        valorDropdown++;
+
+                                        var objDropdown = recuperarObjetoDropdown(valorDropdown, exercicio, item);
+
+                                        exercicio.dropdowns.push(objDropdown);
+                                        break;
+                                    case "INPUT":
+                                        exercicio.questao += "(input)"
+                                        break;
+                                }
+                            }
+
+                            pagina.exercicios.push(exercicio);
+                        }
+                    });
+
+                    console.log("Terminou ");
+                    await recuperarRespostas();
+                    console.log("Terminou 2");
                 }
+
+                await recuperarQuestoes();
+
+                return pagina;
+
             });
 
-            console.log("Terminou ");
-            await recuperarRespostas();
-            console.log("Terminou 2");
+            await browser.close();
+
+            paginas.push(data);
+
+            console.log(`Fim Scrapping: ${url}`);
+        });
+
+        let contador = 1;
+        while (true) {
+
+            if (paginas.length == urls_selection.length) {
+                resolve(paginas);
+                return;
+            }
+
+            await utils.delay(1000)
+            console.log(`Segundos ${contador++}`);
         }
-
-        await recuperarQuestoes();
-
-        return pagina;
-
     });
+}
 
-    console.log(data.exercicios[0].dropdowns[0]);
-
-    await browser.close();
-})();
+module.exports = { recuperarQuestoesSelection }
